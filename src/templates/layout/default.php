c82d8433e7dfa7909b749c424da481984cea6323
Daniel Tiringer <tiringerdaniel@tutanota.com>
2021-08-08 15:23:47 +0200

    Revamp views, only show vue

--- src/templates/layout/default.php
+++ src/templates/layout/default.php
@@ -1,20 +1,8 @@
 <?php
 /**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       https://opensource.org/licenses/mit-license.php MIT License
  * @var \App\View\AppView $this
  */
-
-$cakeDescription = 'CakePHP: the rapid development php framework';
+$cakeDescription = 'CakeVue Application';
 ?>
 <!DOCTYPE html>
 <html>
@@ -30,31 +18,15 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
 
     <?= $this->Html->css(['normalize.min', 'milligram.min', 'cake']) ?>
-    <?= $this->AssetMix->css('app.css'); ?>
+    <?= $this->AssetMix->css('main') ?>
+    <?= $this->AssetMix->script('app') ?>
 
     <?= $this->fetch('meta') ?>
     <?= $this->fetch('css') ?>
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
-    <main class="main">
-        <div class="container">
-            <?= $this->Flash->render() ?>
-            <?= $this->fetch('content') ?>
-            <div class="app"></div>
-        </div>
-    </main>
-    <footer>
-    </footer>
-    <?= $this->AssetMix->script('app'); ?>
+    <?= $this->Flash->render() ?>
+    <?= $this->fetch('content') ?>
 </body>
 </html>
